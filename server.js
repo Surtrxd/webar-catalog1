9ca6cdc420edde90e0f6bef00ae58c9b0cacb012
Surtrxd <danicar.game@gmail.com>
2025-11-26 17:33:57 +0300

    First

---
+++ server.js
@@ -0,0 +1,21 @@
+// server.js
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Отдаём статический фронт
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
